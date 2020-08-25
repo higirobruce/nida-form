a43e8cac5ee504c9bdcd718b6b4b196aac88c15d
brulyn <higirobru@gmail.com>
2020-08-25 20:40:29 +0200

    changes aug 2020

--- src/nidaForm.js
+++ src/nidaForm.js
@@ -5,7 +5,8 @@ import {
   Heading,
   Pane,
   Text,
-  Spinner
+  Spinner,
+  Avatar,
 } from "evergreen-ui";
 import axios from "axios";
 class NidaForm extends Component {
@@ -17,8 +18,16 @@ class NidaForm extends Component {
       forename: "",
       sirnames: "",
       dob: "",
+      img_url: "",
+      province: "",
+      district: "",
+      sector: "",
+      village: "",
+      cell: "",
+      gender: "",
+      place_of_issue: "",
       data_fetched: false,
-      fetching: false
+      fetching: false,
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -27,7 +36,7 @@ class NidaForm extends Component {
   handleChange(e) {
     // console.log(e.target.value);
     this.setState({
-      nid: e.target.value
+      nid: e.target.value,
     });
   }
 
@@ -35,32 +44,66 @@ class NidaForm extends Component {
     // console.log(this.state.nid);
     this.setState({
       data_fetched: false,
-      fetching: true
+      fetching: true,
     });
     let nid = this.state.nid;
     let code = "103";
-    axios.get("http://localhost:3500/?doc=" + nid + "&code=" + code).then(d => {
-      console.log(d.data);
-      if (d.data.result.AuthenticateDocumentResult !== null) {
-        this.setState({
-          forename: d.data.result.AuthenticateDocumentResult.ForeName,
-          sirnames: d.data.result.AuthenticateDocumentResult.Surnames,
-          dob: d.data.result.AuthenticateDocumentResult.DateOfBirth,
-          data_fetched: true,
-          fetching: false,
-          info: d.data.result.AuthenticateDocumentResult
-        });
-      } else {
-        this.setState({
-          forename: "-",
-          sirnames: "-",
-          dob: "-",
-          data_fetched: true,
-          fetching: false,
-          info: d.data.result.AuthenticateDocumentResult
-        });
-      }
-    });
+    axios
+      .get("http://localhost:3500/api/?doc=" + nid + "&code=" + code)
+      // .get("https://nida-node.herokuapp.com/?doc=" + nid + "&code=" + code)
+      .then((d) => {
+        console.log(d);
+
+        if (d.data.result !== null) {
+          if (d.data.result.AuthenticateDocumentResult !== null) {
+            this.setState({
+              forename: d.data.result.AuthenticateDocumentResult.ForeName,
+              sirnames: d.data.result.AuthenticateDocumentResult.Surnames,
+              dob: d.data.result.AuthenticateDocumentResult.DateOfBirth,
+              img_url: d.data.result.Photo,
+              province: d.data.result.Province,
+              district: d.data.result.District,
+              sector: d.data.result.Sector,
+              village: d.data.result.Village,
+              cell: d.data.result.Cell,
+              gender: d.data.result.Sex,
+              place_of_issue: d.data.result.Photo,
+              data_fetched: true,
+              fetching: false,
+              info: d.data.result.AuthenticateDocumentResult,
+            });
+          }
+        } else {
+          this.setState({
+            forename: "-",
+            sirnames: "-",
+            dob: "-",
+            img_url: "-",
+            province: "-",
+            district: "-",
+            sector: "-",
+            village: "-",
+            cell: "-",
+            gender: "-",
+            data_fetched: true,
+            fetching: false,
+            info: d.data.result.AuthenticateDocumentResult,
+          });
+        }
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+
+          this.setState({
+            fetching: false,
+            con_error: true,
+            con_error_text: error.response.data,
+          });
+        }
+      });
   }
 
   render() {
@@ -99,13 +142,42 @@ class NidaForm extends Component {
           )}
 
           {this.state.data_fetched && (
+            <div>
+              <Pane>
+                <Text size={300}>Forename: {this.state.forename}</Text>
+                <br />
+                <Text size={300}>Surnames: {this.state.sirnames}</Text>
+                <br />
+                <Text size={300}>Date Of Birth: {this.state.dob}</Text>
+                <br />
+                <Text size={300}>Province: {this.state.province}</Text>
+                <br />
+                <Text size={300}>District: {this.state.district}</Text>
+                <br />
+                <Text size={300}>Sector: {this.state.sector}</Text>
+                <br />
+                <Text size={300}>Village: {this.state.village}</Text>
+                <br />
+                <Text size={300}>Gender: {this.state.gender}</Text>
+                <br />
+                <Text size={300}>
+                  Place of issue: {this.state.place_of_issue}
+                </Text>
+                <br />
+              </Pane>
+              <Pane>
+                <Avatar
+                  src={this.state.img_url}
+                  name={this.state.forename + " " + this.state.sirnames}
+                  size={100}
+                />
+              </Pane>
+            </div>
+          )}
+
+          {this.state.con_error && (
             <Pane>
-              <Text size={300}>Forename: {this.state.forename}</Text>
-              <br />
-              <Text size={300}>Surnames: {this.state.sirnames}</Text>
-              <br />
-              <Text size={300}>Date Of Birth: {this.state.dob}</Text>
-              <br />
+              <Text size={300}>{this.state.con_error_text}</Text>
             </Pane>
           )}
         </Pane>
